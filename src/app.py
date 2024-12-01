9fb32401d3ee9e27c859723157cc3ff0c53a3390
Antonio Giuseppe Doronzo <antoniodoronzo7@gmail.com>
2024-12-01 01:54:51 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,10 +1,16 @@
+from io import BytesIO
+import io
 from flask import Flask, request, jsonify
 from PIL import Image
 import base64
+from transformers import pipeline
 import os
+import numpy as np
+from pydub import AudioSegment
+from scipy.io import wavfile
 
 app = Flask(__name__)
-#whisper_model = pipeline("automatic-speech-recognition", model="openai/whisper-small")
+whisper_model = pipeline("automatic-speech-recognition", model="openai/whisper-small")
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -18,23 +24,28 @@ def upload():
     text = ''
     
     if has_image:
-      image_b64 = request.files['image']
+      image_b64 = data['image']
 
       try:
-        image_data = base64.b64encode(image_b64)
+        image_data = base64.b64decode(image_b64)
       except Exception as _:
         return jsonify({'error': 'Invalid image file'}), 400
     
     if has_audio:
-      audio_b64 = request.files['audio']
+      audio_b64 = data['audio']
       try:
-        audio_data = base64.b64encode(audio_b64)
-        
-        #result = whisper_model(audio_path)
-        #text = result['text']
+        audio_data = base64.b64decode(audio_b64)
+
+        # Create a byte buffer
+        audio_buffer = io.BytesIO(audio_data)
+
+        # Use Whisper's `transcribe` method with the buffer
+        result = whisper_model(audio_buffer)
+
+        text = result['text']
         text = 'test'
       except Exception as e:
-        return jsonify({'error': 'Error occurred while processing audio file'}), 500
+        return jsonify({'error': f'Error occurred while processing audio file: {str(e)}'}), 500
     else:
       text = data['text']
 
