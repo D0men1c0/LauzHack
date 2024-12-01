15516b4c6b1ede4cd3934b45727262dce815d911
Matteo Balice <matteobalice02@gmail.com>
2024-12-01 08:22:12 +0100

    port 80

--- src/app.py
+++ src/app.py
@@ -81,4 +81,4 @@ def upload():
     return jsonify({'OK': 'Data uploaded', 'trascription': text}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=80)
